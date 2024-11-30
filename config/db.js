258c416cfd3c3ea7b5ee20a203649076d19afe0b
barkeeya01 <barkeeyakhalifa123@gmail.com>
2024-11-30 01:59:11 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ var db;
 try {
   const client = new MongoClient(process.env.DB_URL, {tls:true});
   db = client.db("Afterschooldb");
-  console.log("Connected to database.");
+  console.log("Connected to database from new app.");
 } catch {
   console.log("Failed to connect to database.");
 }
